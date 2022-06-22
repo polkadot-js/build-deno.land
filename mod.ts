8f40cf5b7060a85c942ec0bd189ebca1198e86dd
Jaco <jacogr@gmail.com>
2022-06-22 08:12:36 +0300

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,4 @@
+// Copyright 2017-2022 @polkadot/deno authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+throw new Error('You should not access https://deno.land/x/polkadot/mod.ts directly, rather you should import one of the modules contained within. An example would be https://deno.land.x/polkadot/util/mod.ts');
