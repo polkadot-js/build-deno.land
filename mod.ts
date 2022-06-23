2f4f7f519bec8d5a0e869567d763b7dec8f2c1e8
Jaco <jacogr@gmail.com>
2022-06-23 10:00:37 +0300

    Update root import error message

--- mod.ts
+++ mod.ts
@@ -1,4 +1,12 @@
 // Copyright 2017-2022 @polkadot/deno authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-throw new Error('You should not access https://deno.land/x/polkadot@0.0.0-8/mod.ts directly, rather you should import one of the modules contained within. An example would be https://deno.land.x/polkadot/util/mod.ts');
+throw new Error(`
+  FATAL: Root import found.
+
+  You should not import "https://deno.land/x/polkadot@0.0.0-8/mod.ts" directly,
+  you should rather import from a sub-path for any of the exposed interfaces. 
+
+  An example of this, should you wish to access "util", would be to change your
+  import to "https://deno.land/x/polkadot/util/mod.ts"
+`);
