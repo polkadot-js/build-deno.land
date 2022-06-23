be721bffb15ebec8f49a805d438f2de6e6ebfd64
Jaco <jacogr@gmail.com>
2022-06-23 12:31:56 +0300

    Auto-create root mod.ts

--- mod.ts
+++ mod.ts
@@ -1,12 +1,36 @@
 // Copyright 2017-2022 @polkadot/deno authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-throw new Error(`
-  FATAL: Root import found.
-
-  You should not import "https://deno.land/x/polkadot@0.0.0-8/mod.ts" directly,
-  you should rather import from a sub-path for any of the exposed interfaces. 
-
-  An example of this, should you wish to access "util", would be to change your
-  import to "https://deno.land/x/polkadot/util/mod.ts"
-`);
+export * as api from './api/mod.ts';
+export * as apiAugment from './api-augment/mod.ts';
+export * as apiBase from './api-base/mod.ts';
+export * as apiContract from './api-contract/mod.ts';
+export * as apiDerive from './api-derive/mod.ts';
+export * as hwLedger from './hw-ledger/mod.ts';
+export * as hwLedgerTransports from './hw-ledger-transports/mod.ts';
+export * as keyring from './keyring/mod.ts';
+export * as networks from './networks/mod.ts';
+export * as phishing from './phishing/mod.ts';
+export * as rpcAugment from './rpc-augment/mod.ts';
+export * as rpcCore from './rpc-core/mod.ts';
+export * as rpcProvider from './rpc-provider/mod.ts';
+export * as types from './types/mod.ts';
+export * as typesAugment from './types-augment/mod.ts';
+export * as typesCodec from './types-codec/mod.ts';
+export * as typesCreate from './types-create/mod.ts';
+export * as typesKnown from './types-known/mod.ts';
+export * as util from './util/mod.ts';
+export * as utilCrypto from './util-crypto/mod.ts';
+export * as wasmBridge from './wasm-bridge/mod.ts';
+export * as wasmCrypto from './wasm-crypto/mod.ts';
+export * as wasmCryptoAsmjs from './wasm-crypto-asmjs/mod.ts';
+export * as wasmCryptoInit from './wasm-crypto-init/mod.ts';
+export * as wasmCryptoWasm from './wasm-crypto-wasm/mod.ts';
+export * as wasmUtil from './wasm-util/mod.ts';
+export * as xBigint from './x-bigint/mod.ts';
+export * as xFetch from './x-fetch/mod.ts';
+export * as xGlobal from './x-global/mod.ts';
+export * as xRandomvalues from './x-randomvalues/mod.ts';
+export * as xTextdecoder from './x-textdecoder/mod.ts';
+export * as xTextencoder from './x-textencoder/mod.ts';
+export * as xWs from './x-ws/mod.ts';
