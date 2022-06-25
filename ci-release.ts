49417ff6b9ec196aa7f0de723c91111168aff2c6
Jaco <jacogr@gmail.com>
2022-06-25 15:55:39 +0200

    Tighten up git typings, simplify version regex (#13)
    * Tighten up git typings, simplify version regex
    
    * Update ci-release.ts

--- ci-release.ts
+++ ci-release.ts
@@ -6,8 +6,18 @@
 
 import { stringCamelCase } from 'https://deno.land/x/polkadot@0.0.0-9/util/mod.ts';
 
+// tighter specification for git arguments
+type GitArgs =
+  ['add', '--all', '.'] |
+  ['checkout', 'master'] |
+  ['commit', '--no-status', '--quiet', '-m', string] |
+  ['config', 'merge.ours.driver' | 'push.default' | `user.${'email' | 'name'}`, string] |
+  ['push', string] |
+  ['push', string, '--tags'] |
+  ['tag', string];
+
 // regex for matching `deno.land/x/polkadot[@<version>]/
-const reVer = /deno\.land\/x\/polkadot(@\d\d?\d?\.\d\d?\d?\.\d\d?\d?-?\d?\d?\d?)?\//g;
+const RE_PKG = /deno\.land\/x\/polkadot(@\d*\.\d*\.\d*(-\d*)?)?\//g;
 
 // execute a command
 async function exec(...cmd: string[]): Promise<void> {
@@ -22,10 +32,15 @@ async function exec(...cmd: string[]): Promise<void> {
   };
 }
 
+// shortcut for exec('git', ...string[])
+function git (...args: GitArgs): Promise<void> {
+  return exec('git', ...args);
+}
+
 // retrieve the current version from CHANGELOG.md
 async function getVersion(): Promise<string> {
   const contents = await Deno.readTextFile('CHANGELOG.md');
-  const vermatch = contents.match(/# CHANGELOG\n\n## (\d\d?.\d\d?.\d\d?-?\d?\d?) /);
+  const vermatch = contents.match(/# CHANGELOG\n\n## (\d*.\d*.\d*(-\d*)?) /);
 
   if (!vermatch || !vermatch[1]) {
     throw new Error('FATAL: Unable to extract expected version from CHANGELOG.md');
@@ -43,8 +58,8 @@ async function setVersion(version: string, dir: string): Promise<void> {
       const path = `${dir}/${entry.name}`;
       const contents = await Deno.readTextFile(path);
 
-      if (reVer.test(contents)) {
-        await Deno.writeTextFile(path, contents.replace(reVer, `deno.land/x/polkadot@${version}/`));
+      if (RE_PKG.test(contents)) {
+        await Deno.writeTextFile(path, contents.replace(RE_PKG, `deno.land/x/polkadot@${version}/`));
       }
     }
   }
@@ -55,22 +70,22 @@ async function gitSetup(): Promise<void> {
   const USER = 'github-actions[bot]';
   const MAIL = '41898282+github-actions[bot]@users.noreply.github.com';
 
-  await exec('git', 'config', 'user.name', USER);
-  await exec('git', 'config', 'user.email', MAIL);
-  await exec('git', 'config', 'push.default', 'simple');
-  await exec('git', 'config', 'merge.ours.driver', 'true');
-  await exec('git', 'checkout', 'master');
+  await git('config', 'user.name', USER);
+  await git('config', 'user.email', MAIL);
+  await git('config', 'push.default', 'simple');
+  await git('config', 'merge.ours.driver', 'true');
+  await git('checkout', 'master');
 }
 
 // commit and push the changes to git
 async function gitPush(version: string): Promise<void> {
   const REPO = `https://${Deno.env.get('GH_PAT')}@github.com/${Deno.env.get('GITHUB_REPOSITORY')}.git`;
 
-  await exec('git', 'add', '--all', '.');
-  await exec('git', 'commit', '--no-status', '--quiet', '-m', `[CI Skip] deno.land/x/polkadot@${version}`);
-  await exec('git', 'push', REPO);
-  await exec('git', 'tag', version);
-  await exec('git', 'push', REPO, '--tags');
+  await git('add', '--all', '.');
+  await git('commit', '--no-status', '--quiet', '-m', `[CI Skip] deno.land/x/polkadot@${version}`);
+  await git('push', REPO);
+  await git('tag', version);
+  await git('push', REPO, '--tags');
 }
 
 // creates a new mod.ts file with all the available imports
