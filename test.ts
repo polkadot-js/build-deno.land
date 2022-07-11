f9cd171eb73b9f5a91378d357e33a3055c6daa1f
Jaco <jacogr@gmail.com>
2022-07-11 14:59:03 +0200

    README fixes (#32)

--- test.ts
+++ test.ts
@@ -1,3 +1,6 @@
+// Copyright 2017-2022 @polkadot/deno authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 
 import { stringCamelCase } from './util/mod.ts';
